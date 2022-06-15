c290d9bb305ac89fae664356caeb8d6ff2f3ec59
Usi Mlonzi <usimancobo@gmail.com>
2022-06-15 16:12:22 +0200

    first commit

---
+++ src/components/Details.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
+import './css/Details.css';
+
+const Details = () => {
+
+    const [logindata, setLoginData] = useState([]);
+
+
+    const history = useNavigate();
+
+    const [show, setShow] = useState(false);
+
+    var todayDate = new Date().toISOString().slice(0, 10);
+  
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const Birthday = () => {
+        const getuser = localStorage.getItem("user_login");
+        if (getuser && getuser.length) {
+            const user = JSON.parse(getuser);
+         
+            setLoginData(user);
+
+
+            const userbirth = logindata.map((el, k) => {
+                return el.date === todayDate
+            });
+
+            if (userbirth) {
+                setTimeout(() => {
+                    console.log("ok");
+                    handleShow();
+                }, 3000)
+            }
+        }
+    }
+
+    const userlogout = ()=>{
+        localStorage.removeItem("user_login")
+        history("/");
+    }
+
+    useEffect(() => {
+        Birthday();
+    }, [])
+
+    return (
+        <>
+            {
+                logindata.length === 0 ? "errror" :
+                    <>
+                        <h1 className='EmployeeHeader'>Employee Directory</h1> <br /> <br />
+                        <div className='info'><h2>{logindata[0].name}</h2> <h2>{logindata[0].email}</h2> <h2>{logindata[0].date}</h2> <h1>{logindata[0].name}</h1>
+                        </div>
+                        {/* <div><h1>{logindata[1].name}</h1> <h1>{logindata[1].email}</h1> <h1>{logindata[0].date}</h1> <h1>{logindata[0].name}</h1>
+                        </div> */}
+                        <Button onClick={userlogout}>LogOut</Button>
+
+                {/* {
+                    logindata[0].date === todayDate ? 
+                    <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>{logindata[0].name} 😄</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>Wish you many many happy returns of the day ! 🍰</Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                </Button>
+                                <Button variant="primary" onClick={handleClose}>
+                                    Save Changes
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>:""
+                }    */}
+                     
+                    </>
+            }
+        </>
+    )
+}
+
+export default Details
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
